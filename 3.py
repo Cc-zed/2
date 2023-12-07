ab38ba7c0da2986236616598b17cfb17f96d6e87
cc-z <152991415+Cc-zed@users.noreply.github.com>
2023-12-07 17:24:44 +0200

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,77 @@
+# -----------------------------Task 1------------------------------------
+
+# first_num = int(input("----Enter first number: "))
+# Second_num = int(input("----Enter second number: "))
+# Third_num = int(input("----Enter third number: "))
+
+# print("\n---1 - sum of numbers, 2 - mnozennya of numbers---")
+
+# ch = int(input("----Enter number: "))
+
+# match ch:
+#     case 1: print("\n-----Sum of numbers: ", first_num+Second_num+Third_num)
+#     case 2: print("\n-----Mnozennya of numbers: ", first_num*Second_num*Third_num)
+
+
+
+# Comment to task 1:
+#           <------- Easy
+
+
+
+# -----------------------------Task 2------------------------------------
+
+# first_num = int(input("----Enter first number: "))
+# Second_num = int(input("----Enter second number: "))
+# Third_num = int(input("----Enter third number: "))
+
+# print("\n---1 - max of numbers, 2 - min of numbers, 3 - serednie arefmetuchne---")
+
+# ch = int(input("----Enter number: "))
+# if ch == 1:
+#     if Second_num < Third_num and first_num < Third_num:
+#         print(f"\n-----{Third_num} is biggest one-----")
+#     if Third_num < first_num and Second_num < first_num:
+#         print(f"\n-----{first_num} is biggest one-----")
+#     if first_num < Second_num and Third_num < Second_num:
+#         print(f"\n-----{Second_num} is biggest one-----")
+
+# if ch == 2:
+#     if Second_num > Third_num and first_num > Third_num:
+#         print(f"\n-----{Third_num} is smallest one-----")
+#     if Third_num > first_num and Second_num > first_num:
+#         print(f"\n-----{first_num} is smallest one-----")
+#     if first_num > Second_num and Third_num > Second_num:
+#         print(f"\n-----{Second_num} is smallest one-----")
+
+# if ch == 3:
+#     print("\n-----Serednie arefmetuchne: ",(first_num+Second_num+Third_num)/3,"-----")
+
+
+
+# Comment to task 2:
+#           <------- Easy too
+
+
+
+# -----------------------------Task 3------------------------------------
+
+# num = int(input("----Enter your value: "))
+
+# print("\n---1 - To miles, 2 - To inches, 3 - To yards---")
+
+# ch = int(input("----Enter number: "))
+
+# match ch:
+#     case 1: print("-----Meters in miles:", num * 0.000621371,"-----")
+#     case 2: print("-----Meters in inches:", num * 39.3701,"-----")
+#     case 3: print("-----Meters in yards", num *1.09361,"-----")
+
+
+
+# Comment to task 3:
+#           <------- I converted everything in google convert, so idk if its right
+
+
+
+# ---------------------------------END------------------------------------
---
+++ 4.py
@@ -0,0 +1,102 @@
+# -----------------------------Task 1------------------------------------
+
+# num = int(input("----Enter your number: "))
+
+# match num:
+#     case 1: print("----- Monday -----")
+#     case 2: print("----- Tuesday -----")
+#     case 3: print("----- Wednesday -----")
+#     case 4: print("----- Thursday -----")
+#     case 5: print("----- Friday -----")
+#     case 6: print("----- Saturday -----")
+#     case 7: print("----- Sunday -----")
+
+
+
+# Comment to task 1:
+#           <------- This one was easiest
+
+
+
+# -----------------------------Task 2------------------------------------
+
+# num = int(input("----Enter your number: "))
+
+# match num:
+#     case 1: print("----- January -----")
+#     case 2: print("----- February -----")
+#     case 3: print("----- March -----")
+#     case 4: print("----- April -----")
+#     case 5: print("----- May -----")
+#     case 6: print("----- June -----")
+#     case 7: print("----- July -----")
+#     case 8: print("----- August -----")
+#     case 9: print("----- September -----")
+#     case 10: print("----- October -----")
+#     case 11: print("----- November -----")
+#     case 12: print("----- December -----")
+
+
+
+# Comment to task 2:
+#           <------- This one took more time only because of printing all months
+
+
+
+# -----------------------------Task 3------------------------------------
+
+# num = int(input("----Enter your number: "))
+
+# #       NEGATIVE
+# if num < 0:
+#     print("----- Number is negative -----")
+# #       POSITIVE
+# if num > 0:
+#     print("----- Number is Positive -----")
+# #       EQUAL
+# if num == 0:
+#     print("----- Number is equal zero -----")
+
+
+
+# Comment to task 3:
+#           <------- Where are all hard tasks?
+
+
+
+# -----------------------------Task 4------------------------------------
+
+# first_num = int(input("----Enter first number: "))
+# Second_num = int(input("----Enter second number: "))
+
+# if first_num == Second_num:
+#     print("Numbers are equal")
+
+
+# #     VARIANT 1
+
+
+# if first_num < Second_num:
+#     print("----- From smallest to biggest number:\n---", first_num, ",", Second_num)
+
+# if first_num > Second_num:
+#     print("----- From smallest to biggest number:\n---", Second_num, ",", first_num)
+
+
+# #     VARIANT 2
+
+
+# if first_num < Second_num:
+#     print(f"----- From smallest to biggest number:\n--- {first_num}, {Second_num} ---")
+
+# if first_num > Second_num:
+#     print(f"----- From smallest to biggest number:\n--- {Second_num}, {first_num} ---")
+
+
+
+# Comment to task 4:
+#           <------- This is too easy.
+
+
+
+# ---------------------------------END------------------------------------
